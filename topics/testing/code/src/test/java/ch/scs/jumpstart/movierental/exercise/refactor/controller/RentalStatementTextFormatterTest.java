4d25522a855fe4188f3934e8b7e8b9b443c2ea5b
Lucius Bachmann <lucius.bachmann@scs.ch>
2022-02-03 11:58:57 +0100

    + testing: add more verbose help for the movierental exercise
    Issue: #31

---
+++ topics/testing/code/src/test/java/ch/scs/jumpstart/movierental/exercise/refactor/controller/RentalStatementTextFormatterTest.java
@@ -0,0 +1,30 @@
+package ch.scs.jumpstart.movierental.exercise.refactor.controller;
+
+import ch.scs.jumpstart.movierental.exercise.refactor.rentalstatement.RentalStatement;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RentalStatementTextFormatterTest {
+
+  private static final String CUSTOMER_NAME = "customer1";
+  private static final RentalStatement EMPTY_STATEMENT =
+      RentalStatementBuilder.builder(CUSTOMER_NAME).build();
+
+  private static final RentalStatement STATEMENT_WITH_LINES =
+      RentalStatementBuilder.builder(CUSTOMER_NAME)
+          .withStatementMovie("movie1", 1.1f)
+          .withStatementMovie("movie2", 100.2f)
+          .build();
+  private RentalStatementTextFormatter rentalStatementTextFormatter;
+
+  @BeforeEach
+  public void setup() {
+    rentalStatementTextFormatter = new RentalStatementTextFormatter();
+  }
+
+  @Test
+  public void format_empty_statement() {}
+
+  @Test
+  public void format_lines() {}
+}
