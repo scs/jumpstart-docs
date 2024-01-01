519053281ed56b8e238db0ce910dc6b9158a45de
BacLuc <lucius.bachmann@gmx.ch>
2024-01-01 23:05:07 +0100

    linux: use systemctl instead of service in update-hello-world-service.sh
    That it also supports systems without the service abstraction layer.

--- topics/linux/code/solution/update-hello-world-service.sh
+++ topics/linux/code/solution/update-hello-world-service.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR=$(dirname $(realpath $0))
 
 hello_world_versions_dir=$(realpath $SCRIPT_DIR/../hello_world_service)
 last_file=$(ls -t $hello_world_versions_dir | head -1)
-sudo service hello-world stop
+sudo systemctl stop hello-world
 sudo cp $hello_world_versions_dir/$last_file /usr/local/bin/hello-world
 sudo chmod +x /usr/local/bin/hello-world
-sudo service hello-world start
+sudo systemctl start hello-world
